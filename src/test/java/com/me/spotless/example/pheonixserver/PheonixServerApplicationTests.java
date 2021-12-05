f49dfc0b08a17a5ba346478b76a979566942c590
Faraz <58445945+farazahmadk@users.noreply.github.com>
2021-12-05 08:52:22 -0300

    Integrate Spotless with Google Java Formatter

---
+++ src/test/java/com/me/spotless/example/pheonixserver/PheonixServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.spotless.example.pheonixserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PheonixServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
